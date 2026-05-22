850d94939a7a89855757aa398884a62ce4efc697
oscargatau-eng <oscargatau@gmail.com>
2026-05-22 10:36:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,7 @@ st.write(
 )
 import streamlit as st
 
-action = st.menu_button("Export", options=["CSV", "JSON", "PDF"])
-if action == "CSV":
-    st.write("Exporting as CSV...")
-elif action == "JSON":
-    st.write("Exporting as JSON...")
-elif action == "PDF":
-    st.write("Exporting as PDF...")
+st.page_link("your_app.py", label="Home", icon="🏠")
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
