2cfd2a1f7f63227182568ba1fa2d3befbd7532d3
oscargatau-eng <oscargatau@gmail.com>
2026-05-22 10:47:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ import streamlit as st
 
 st.caption("Jomok hepy memang pilihan suki.")
 st.caption("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH")
+import streamlit as st
+
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[bad] :angry face:")
