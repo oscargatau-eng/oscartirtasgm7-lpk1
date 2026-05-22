c3fc2fa0dd2441b51cb735ed88ff8be36e0e06db
oscargatau-eng <oscargatau@gmail.com>
2026-05-22 10:49:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ st.caption("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH")
 import streamlit as st
 
 st.title("This is a title")
-st.title("_Streamlit_ is :blue[bad] :finger:")
+st.title("_Streamlit_ is :blue[bad] :car:")
