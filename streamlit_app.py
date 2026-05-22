b32b9560231a3cbb98c188410bb0afa16fd254b3
oscargatau-eng <oscargatau@gmail.com>
2026-05-22 10:42:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ st.write(
 )
 import streamlit as st
 
-st.caption("This is a string that explains something above.")
+st.caption("Jomok hepy memang pilihan suki.")
 st.caption("A caption with _italics_ :blue[colors] and emojis :sunglasses:")
