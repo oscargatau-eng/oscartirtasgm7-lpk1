d97f42de7dc93dc16abb21da94daa0c16378bf84
oscargatau-eng <oscargatau@gmail.com>
2026-05-22 10:38:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,25 @@ st.title("🎈 oscar")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import pandas as pd
+import streamlit as st
+
+data_df = pd.DataFrame(
+    {
+        "widgets": ["st.selectbox", "st.number_input", "st.text_area", "st.button"],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "widgets": st.column_config.TextColumn(
+            "Widgets",
+            help="Streamlit **widget** commands 🎈",
+            default="st.",
+            max_chars=50,
+            validate=r"^st\.[a-z_]+$",
+        )
+    },
+    hide_index=True,
+)
