0732d5d93dddb2c5ab6287d282b34df25c322788
oscargatau-eng <oscargatau@gmail.com>
2026-05-22 10:35:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 oscar")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+action = st.menu_button("Export", options=["CSV", "JSON", "PDF"])
+if action == "CSV":
+    st.write("Exporting as CSV...")
+elif action == "JSON":
+    st.write("Exporting as JSON...")
+elif action == "PDF":
+    st.write("Exporting as PDF...")
