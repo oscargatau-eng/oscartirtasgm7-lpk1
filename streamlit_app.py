21c335f0c5a54f78cc10ee8568bcdce50f6ccba2
oscargatau-eng <oscargatau@gmail.com>
2026-05-22 10:30:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Ape pulak si bodo ni")
+st.title("🎈 Ape pulak si bodo ni hhehehehe")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
